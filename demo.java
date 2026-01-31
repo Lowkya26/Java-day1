e162211f4ad0bb4a6dfbe138d5a1268972a612b4
Lowkya26 <24hr1a0536@mtieat.org>
2026-01-31 14:29:26 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,8 @@
+class demo
+{
+    public static void main(String[] args) {
+        System.out.println("my name is lowkya");
+        System.out.print("how are you all");
+        System.out.println("good afternoon");
+    }
+}
