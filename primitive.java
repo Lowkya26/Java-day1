c295d0e01b35ab527feb6ecccfd25b1abf7c35ac
Lowkya26 <24hr1a0536@mtieat.org>
2026-02-07 10:55:57 +0530

    Add files via upload

---
+++ primitive.java
@@ -0,0 +1,19 @@
+public class primitive {
+    public static void main(String[] args) {
+        int a=5000;
+        byte b=126;
+        short c=200;
+        float d=10.23f;
+        double e=1675438;
+        boolean f=true;
+        char g='L';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+    }
+    
+}
