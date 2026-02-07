9900de357ee0e0a39881a30c5f059558ee997112
Lowkya26 <24hr1a0536@mtieat.org>
2026-02-07 14:28:56 +0530

    Add files via upload

---
+++ uniary.java
@@ -0,0 +1,13 @@
+public class uniary {
+    public static void main(String[] args) {
+        float a=12.67f;
+        System.out.println(+a);
+        System.out.println(-a);
+        System.out.println(++a);
+        System.out.println(a++);
+        System.out.println(a--);
+        System.out.println(--a);
+        }
+    
+}
+
