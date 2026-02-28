d67013f5fe3f6821feb836bb9a8083d74be9bf1e
Lowkya26 <24hr1a0536@mtieat.org>
2026-02-28 14:20:49 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,9 @@
+public class Array {
+    public static void main(String[] args) {
+        int arr[]={10, 20, 30};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            System.out.println("sum:"+(sum+=arr[i]));
+        }
+    }
+} 
